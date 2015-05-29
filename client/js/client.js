3d1a8c659c7f8e900962febcbbb068297f4fe9d5
Ogamedia <kwame.yeboah@meltwater.org>
2015-05-29 23:40:43 +0000

    added search route

--- client/js/client.js
+++ client/js/client.js
@@ -1,8 +1,44 @@
-var smsOptions = {
-	// phoneNumber: parseInt(Details.phoneNumber)
+
+Template.home.events({
+	'click #search': function () {
+		Router.go('/list-of-projects')
+	}
+});
+
+
+
+
+// pick all user details in the data base and count them
+var allUsers = userDetails.find()
+var counter = userDetails.find().count();
+
+// ============sms related options =================== //
+// the message sender name
+var ourCredentials = "TransGov"
+var content = "placeholder content must be  less than 150 characters"
+
+
+// run an if loop if there items in the data base
+if (counter > 0) {
+	for (var i = 0; i < counter; i++) {
+		var userItem = allUsers.fetch()[i];
+		console.log(userItem);
+
+		// get the phonenumber
+		var phoneNumber = userItem.phoneNumber;
+
+		var smsOptions = {
+			From: ourCredentials,
+			phone: phoneNumber,
+			contents: content
+		};
+		// Meteor.call('sendMessage', smsOptions);
+	};
+
 };
 
 
 
+
+
 // call for sms message
-// Meteor.call('sendMessage', smsOptions);
--- server/sms.js
+++ server/sms.js
@@ -1,24 +1,24 @@
-//   Meteor.methods({sendMessage: function (smsOptions) {
-//     this.unblock();
-//     try {
-//       var result = HTTP.call("GET", "https://api.smsgh.com/v3/messages/send?",
-//       {
-//         params:
-//         {
-//           From: smsOptions.From,
-//           To: smsOptions.phoneNumber,
-//           Content: smsOptioons.contents,
-//           ClientId: "vradbyxu",
-//           ClientSecret: "ypaolptq",
-//           RegisteredDelivery: true,
-//         }
-//       }
-//       );
-//       return true;
-//     } catch (e) {
-//     // Got a network error, time-out or HTTP error in the 400 or 500 range.
-//     console.log(e);
-//     return false;
-//   }
-// }
-// });
+  Meteor.methods({sendMessage: function (smsOptions) {
+    this.unblock();
+    try {
+      var result = HTTP.call("GET", "https://api.smsgh.com/v3/messages/send?",
+      {
+        params:
+        {
+          From: smsOptions.From,
+          To: smsOptions.phone,
+          Content: smsOptions.contents,
+          ClientId: "vradbyxu",
+          ClientSecret: "ypaolptq",
+          RegisteredDelivery: true,
+        }
+      }
+      );
+      return true;
+    } catch (e) {
+    // Got a network error, time-out or HTTP error in the 400 or 500 range.
+    console.log(e);
+    return false;
+  }
+}
+});
